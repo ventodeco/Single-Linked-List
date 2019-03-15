8c91916a96dff79cd1e281b81c6e7a31226316b8
ventodeco <ventodecoboagoes@gmail.com>
2019-03-15 09:36:16 +0700

    menambah kerangka adt Single Linked List dengan C++

---
+++ header/singlelinklist.h
@@ -0,0 +1,15 @@
+#ifndef SINGLELINKLIST_H_INCLUDED
+#define SINGLELINKLIST_H_INCLUDED
+
+#define Nil NULL
+#define first(L) ((L).first)
+#define next(P) (P)->next
+#define info(P) (P)->info
+
+using namespace std;
+#include <iostream>
+
+
+
+
+#endif // SINGLELINKLIST_H_INCLUDED
---
+++ source code/main.cpp
@@ -0,0 +1,6 @@
+#include "singlelinklist.h"
+
+
+int main() {
+	
+}
