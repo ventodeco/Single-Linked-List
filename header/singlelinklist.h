a1ba712317632855ff40e649ff4e92e7c2816ee4
ventodeco <ventodecoboagoes@gmail.com>
2019-03-15 09:55:27 +0700

    memindah main.cpp diluar dan melengkapi file header

--- header/singlelinklist.h
+++ header/singlelinklist.h
@@ -9,7 +9,36 @@
 using namespace std;
 #include <iostream>
 
+struct mahasiswa{
+	string nama;
+	string nim;
+	float ipk;
+};
 
+typedef mahasiswa infotype;
+typedef struct elmlist *address;
+
+struct elmlist{
+	infotype info;
+	address next;
+};
+
+struct list{
+	address first;
+};
+
+
+void CreateList(list &L);
+address alokasi(infotpe x);
+void insertFirst(list &L, address P);
+void insertLast(list &L, address P);
+void insertAfter(list &L, address P);
+void deleteFirst(list &L, address &P);
+void deleteLast(list &L, address &P);
+void deleteAfter(list &L, address &P);
+void sortbyIPK(list &L);
+void sortbyNama(list &L);
+void tampilkan(list L);
 
 
 #endif // SINGLELINKLIST_H_INCLUDED
